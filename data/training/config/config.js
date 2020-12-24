779618be23c54d4c1a2fb40d0802dd46386b3666
ray <33544674+conver4y@users.noreply.github.com>
2020-12-25 03:28:05 +0800

    Update config.js

--- data/training/config/config.js
+++ data/training/config/config.js
@@ -1,9 +1,15 @@
-{
+let config = {
   iterationState: 25000,
   log: true,
-  threshold: 0.005, 
-  lr: 0.01,
+  threshold: 0.0025, 
+  lr: 0.01, // Perfect rate, 1e-4.
   momentum: 0.9,
   hidden_layers: [23],
-  noPerms_msg: "prohibited:perms:FLAGS"
-}
+  noPerms_msg: "prohibited:perms:FLAGS",
+  input: {},
+  output: {},
+  state_between: [2, 178, 413],
+  timeout_training: ['v1', Infinity]
+};
+
+module.exports = config;
