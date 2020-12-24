de67ee4e04f507e3263c775348d0254f0d0aea8b
ray <33544674+conver4y@users.noreply.github.com>
2020-12-25 03:06:33 +0800

    Create gua.js

---
+++ data/dictionary/slang/gua.js
@@ -0,0 +1,23 @@
+var word = [
+  "gue",
+  "gw",
+  "gueh",
+  "gwe",
+  "w",
+  "ane", "aye", "kuring", "abdi", "ayas", "kaule", "titiang", "ikam", "ulon", "kei", "loun", "den", "ambo",
+  "aing",
+  "gua",
+  "ngua",
+  "ngue",
+  "guak",
+  "guhe",
+  "guwa",
+  "guwe",
+  "gwa",
+  'sy',
+  'aku',
+  'q',
+  'i'
+];
+
+module.exports = word;
